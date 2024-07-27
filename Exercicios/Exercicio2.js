d24f46e29ce5aec1a731262c9601cc8a35d5b746
Pedro Viana <pedro.viana@sete.inf.br>
2024-07-27 17:28:03 -0300

    Enviando mais exercícios

---
+++ Exercicios/Exercicio2.js
@@ -0,0 +1,7 @@
+let idade = prompt('Qual a sua idade?')
+parseInt(idade)
+if(idade >= 18){
+    console.log('Pode entrar')
+}else{
+    console.log('Ainda não pode entrar')
+}
---
+++ Exercicios/Exercicio3.js
@@ -0,0 +1,7 @@
+const nome = 'Pedro'
+let verificandoNome = prompt('Digite seu nome')
+if(verificandoNome == nome){
+    console.log('Seja bem-vindo')
+}else{
+    console.log('Pessoa não reconhecida')
+}
