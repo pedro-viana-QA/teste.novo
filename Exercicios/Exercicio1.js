c5a3601cdbef3af7d86c098abf978950ab63825c
Pedro Viana <pedro.viana@sete.inf.br>
2024-07-27 17:13:22 -0300

    Adicionando nova pasta com exercícios do curso Udemy

---
+++ Exercicios/Exercicio1.js
@@ -0,0 +1,7 @@
+const nome = 'Pedro'
+let idade = 24
+const isMale = true
+
+if(isMale == true){
+    console.log(`${nome} tem ${idade} anos`)
+}
--- script.js
+++
@@ -1,8 +0,0 @@
-let numero = 20
-let fatorial = 1
-
-for(i = 1; i < numero; i++){
-    fatorial = fatorial * i
-}
-
-console.log(fatorial, fatorial)
