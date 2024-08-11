e6b2b1a8ba20c5aeb977d1de523d15beb8795b22
Pedro Viana <pedro.viana@sete.inf.br>
2024-08-11 17:34:07 -0300

    Enviando exercicio 8

---
+++ Exercicios/Exercicio8.js
@@ -0,0 +1,24 @@
+//loop para exibir de 100 a 50
+//I'm gonna put below 3 answers to that require using the 3 loop forms 
+
+//while
+console.log('Using while')
+let i = 100
+while(i >= 50){
+    console.log(i)
+    i--
+}
+
+//do while
+let count = 100
+console.log('Using do while')
+do{
+    console.log(count)
+    count--
+}while(count >= 50)
+
+//for
+console.log('for')
+for(let i = 100; i >= 50; i--){
+    console.log(i)
+}    
