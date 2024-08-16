acaa42d5ef0f5140698317065d71aa8e592eb917
Pedro Viana <pedro.viana@sete.inf.br>
2024-08-16 16:49:52 -0300

    Corrigindo código de boas vindas

--- Exercicios/Exercicio3.js
+++ Exercicios/Exercicio3.js
@@ -1,7 +1,7 @@
 const nome = 'Pedro'
 let verificandoNome = prompt('Digite seu nome')
 if(verificandoNome == nome){
-    console.log('Seja bem-vindo')
+    console.log('Seja bem-vindo' + nome)
 }else{
     console.log('Pessoa não reconhecida')
 }
