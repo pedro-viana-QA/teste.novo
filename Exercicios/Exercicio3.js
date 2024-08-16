8d1d0f921fcd98cfb7ade6158736bcf68c15ac33
Pedro Viana <pedro.viana@sete.inf.br>
2024-08-16 16:50:38 -0300

    Separação nome

--- Exercicios/Exercicio3.js
+++ Exercicios/Exercicio3.js
@@ -1,7 +1,7 @@
 const nome = 'Pedro'
 let verificandoNome = prompt('Digite seu nome')
 if(verificandoNome == nome){
-    console.log('Seja bem-vindo' + nome)
+    console.log('Seja bem-vindo ' + nome)
 }else{
     console.log('Pessoa não reconhecida')
 }
